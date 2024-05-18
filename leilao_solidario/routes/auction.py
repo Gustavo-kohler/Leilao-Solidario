04779f769886d9298505e6b5958fddd84097e001
gustavo luiz kohler <gustavoluizkohler@gmail.com>
2024-05-18 11:57:07 -0300

    integraçao com usuario logado

--- leilao_solidario/routes/auction.py
+++ leilao_solidario/routes/auction.py
@@ -1,8 +1,9 @@
 from flask import Blueprint, render_template, abort
+from flask_login import current_user
 
 AUCTION = Blueprint('auction', __name__)
 
 
 @AUCTION.route('/auction')
 def auction():
-    return render_template('auction.html')
+    return render_template('auction.html', current_user=current_user)
