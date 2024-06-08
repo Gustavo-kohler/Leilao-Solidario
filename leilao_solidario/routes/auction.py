ed96757266ab6d5e3539bba699deb1d4f35e2c0f
gustavo luiz kohler <gustavoluizkohler@gmail.com>
2024-06-08 09:36:45 -0300

    filtrando a pagina do leilao pelo id

--- leilao_solidario/routes/auction.py
+++ leilao_solidario/routes/auction.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template, abort
 from flask_login import current_user
+from leilao_solidario.models import Leilao, Usuario
 
 AUCTION = Blueprint('auction', __name__)
 
 
-@AUCTION.route('/auction')
-def auction():
-    return render_template('auction.html', current_user=current_user)
+@AUCTION.route('/auction/<auction_id>', methods=["GET", "POST"])
+def auction(auction_id):
+    auction = Leilao.query.get(auction_id)
+    host = Usuario.query.get(auction.host)
+    return render_template('auction.html', current_user=current_user, auction=auction, host=host)
